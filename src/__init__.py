69cce94218be4522e550da6b962d8aa2ab6146b3
Alexander Fengler <alexanderfengler@gmx.de>
2021-06-17 23:51:48 -0400

    fixing imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,1 @@
 from . import cssm
-
-from . import basic_simulators
-from . import dataset_generators
-from . import config
-from . import support_utils
--- ssms/__init__.py
+++ ssms/__init__.py
@@ -1,1 +1,6 @@
 __version__ = '0.0.1'
+
+from . import basic_simulators
+from . import dataset_generators
+from . import config
+from . import support_utils
