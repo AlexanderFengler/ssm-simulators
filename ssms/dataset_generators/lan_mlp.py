e1abb08d4c311391af0de575e44b3608adc66355
Alexander Fengler <alexanderfengler@gmx.de>
2023-01-04 22:27:47 +0100

    mode function changed behavior in scipy > 1.10

--- ssms/dataset_generators/lan_mlp.py
+++ ssms/dataset_generators/lan_mlp.py
@@ -76,7 +76,7 @@ class data_generator():
             tmp_rts = simulations['rts'][simulations['choices'] == choice_tmp]
             tmp_n_c = len(tmp_rts)
             if tmp_n_c > 0:
-                mode_, mode_cnt_ = mode(tmp_rts)
+                mode_, mode_cnt_ = mode(tmp_rts, keepdims=True)
                 std_ = np.std(tmp_rts)
                 mean_ = np.mean(tmp_rts)
                 if tmp_n_c < 5:
