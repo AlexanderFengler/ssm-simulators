bbfbce91d49f8fc4e59b0ac4e95b3c599630ad2f
Alexander Fengler <alexanderfengler@gmx.de>
2023-09-08 16:50:51 -0400

    towards git actions and proper use of pyproject

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #from distutils.core import setup
-#from Cython.Build import cythonize
-from setuptools import setup
-from setuptools import Extension
+from xml.etree.ElementInclude import include
+from setuptools import setup, Extension
 import numpy
 
 try:
@@ -11,22 +10,26 @@ try:
 except ImportError:
     ext_modules = [Extension('cssm', ['src/cssm.pyx'], language = 'c++')]
 
-
-setup(  
-        name = 'ssm-simulators',
-        version='0.3.1',
-        author = 'Alexander Fenger',
-        url = 'https://github.com/AlexanderFengler/ssms',
-        packages= ['ssms', 'ssms.basic_simulators', 'ssms.config', 'ssms.dataset_generators', 'ssms.support_utils'],
-        description='SSMS is a package collecting simulators and training data generators for a bunch of generative models of interest in the cognitive science / neuroscience and approximate bayesian computation communities',
-        install_requires= ['numpy >= 1.17.0', 'scipy >= 1.6.3', 'cython >= 0.29.23', 'pandas >= 1.0.0', 'scikit-learn >= 0.24.0', 'psutil >= 5.0.0'],
-        setup_requires= ['numpy >= 1.17.0', 'scipy >= 1.6.3', 'cython >= 0.29.23', 'pandas >= 1.0.0', 'scikit-learn >= 0.24.0', 'psutil >= 5.0.0'],
-        include_dirs = [numpy.get_include()] ,
-        ext_modules = ext_modules ,
-        classifiers=[ 'Development Status :: 1 - Planning', 
-                      'Environment :: Console',
-                      'License :: OSI Approved :: MIT License',
-                      'Programming Language :: Python',
-                      'Topic :: Scientific/Engineering'
-                    ]
-    )
+setup(
+    packages=['ssms', 'ssms.basic_simulators', 'ssms.config', 'ssms.dataset_generators', 'ssms.support_utils'],
+    include_dirs = [numpy.get_include()],
+    ext_modules = ext_modules,
+)
+# setup(  
+#         name = 'ssm-simulators',
+#         version='0.3.1',
+#         author = 'Alexander Fenger',
+#         url = 'https://github.com/AlexanderFengler/ssms',
+#         packages= ['ssms', 'ssms.basic_simulators', 'ssms.config', 'ssms.dataset_generators', 'ssms.support_utils'],
+#         description='SSMS is a package collecting simulators and training data generators for a bunch of generative models of interest in the cognitive science / neuroscience and approximate bayesian computation communities',
+#         install_requires= ['numpy >= 1.17.0', 'scipy >= 1.6.3', 'cython >= 0.29.23', 'pandas >= 1.0.0', 'scikit-learn >= 0.24.0', 'psutil >= 5.0.0'],
+#         setup_requires= ['numpy >= 1.17.0', 'scipy >= 1.6.3', 'cython >= 0.29.23', 'pandas >= 1.0.0', 'scikit-learn >= 0.24.0', 'psutil >= 5.0.0'],
+#         include_dirs = [numpy.get_include()] ,
+#         ext_modules = ext_modules ,
+#         classifiers=[ 'Development Status :: 1 - Planning', 
+#                       'Environment :: Console',
+#                       'License :: OSI Approved :: MIT License',
+#                       'Programming Language :: Python',
+#                       'Topic :: Scientific/Engineering'
+#                     ]
+#     )
