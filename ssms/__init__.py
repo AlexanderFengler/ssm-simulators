bee6fa9959b661226c3cf24dea75dd8ade80e881
Alexander Fengler <alexanderfengler@gmx.de>
2023-09-14 16:00:06 -0400

    black ruff again

--- ssms/__init__.py
+++ ssms/__init__.py
@@ -1,9 +1,9 @@
-import importlib.metadata
+# import importlib.metadata
 from . import basic_simulators
 from . import dataset_generators
 from . import config
 from . import support_utils
 
-__version__ = "0.4.4" #importlib.metadata.version(__package__ or __name__)
+__version__ = "0.4.4"  # importlib.metadata.version(__package__ or __name__)
 
 __all__ = ["basic_simulators", "dataset_generators", "config", "support_utils"]
