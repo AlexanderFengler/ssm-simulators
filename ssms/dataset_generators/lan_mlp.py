ba9628f0d7f77b6cec129e8c6a5f04e91b1e65a5
krishnbera <berakrishn@gmail.com>
2024-12-13 22:01:59 -0500

    run black

--- ssms/dataset_generators/lan_mlp.py
+++ ssms/dataset_generators/lan_mlp.py
@@ -362,7 +362,7 @@ class data_generator:
                 Dictionary containing the transformed parameters.
         """
 
-        #print(theta)
+        # print(theta)
 
         if self.model_config["name"] in ["lba_angle_3_v2", "lba_angle_3_v3"]:
             # ensure that a is always greater than z
